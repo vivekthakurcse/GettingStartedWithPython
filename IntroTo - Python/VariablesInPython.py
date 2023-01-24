aef68ae2a564714a6ee103d549eed6256519e494
Vivek Thakur <vivekthakurcse779@gmail.com>
2023-01-24 17:39:46 +0530

    Add files via upload

---
+++ IntroTo - Python/VariablesInPython.py
@@ -0,0 +1,16 @@
+# variables in python
+# variables is nothing more but a container for storing data\values in it. There is np need to declare variable before its use.
+# we dont need to data type here because python understands its data type by itself
+
+# integer variables
+a = 1
+# floating point variables
+b = 1.2345
+# string variables
+name = "vivek"
+print("\n integer = " , a , "\n floating point = " , b , "\n string = " + name)
+
+# Q1> take a string and print its length
+name = "panda"
+
+print("\n name length = " , len(name))
