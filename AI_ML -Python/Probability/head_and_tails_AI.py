f6304e281daea7c0cf4ba07d0286f4cfdf6c990b
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-02-22 00:49:50 +0530

    Add files via upload

--- AI_ML -Python/Probability/head_and_tails_AI.py
+++ AI_ML -Python/Probability/head_and_tails_AI.py
@@ -3,16 +3,21 @@ n = int(input(" Enter No. of Coins  = "))
 outcomes = pow(2,n)
 print(" Number of outcomes = ", outcomes)
 
-for i in range(n+1):
-    num_tails = n - i
-    probability = 1/outcomes
-    print(" Probability of ", i, " heads and", num_tails, "tails = ", probability )
+
 
 # Possibilities
+print("\n All possible outcomes > ")
 for i in range(outcomes):
     
     outcome_str = bin(i)[2:]
     outcome_str = '0'*(n-len(outcome_str)) + outcome_str
     outcome_str = outcome_str.replace('0', ' T ').replace('1', ' H ')
-    print(outcome_str)
-    
+    print()
+    print("\t\t" + outcome_str)
+  
+print("\n")
+for i in range(n+1):
+    num_tails = n - i
+    probability = 1/outcomes
+    print(" Probability of ", i, " heads and", num_tails, "tails = ", probability )
+      
