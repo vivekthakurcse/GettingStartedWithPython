059c52224ad30b5eb97349fa9e7b068237021826
Vivek Thakur <vivek1kumar2thakur@gmail.com>
2023-02-23 19:31:56 +0530

    Add files via upload

--- AI_ML -Python/NLP Based Evaluator/NLP_based_calculator.py
+++ AI_ML -Python/NLP Based Evaluator/NLP_based_calculator.py
@@ -6,14 +6,17 @@ while True :
 	
 	print("\n enter expression ")
 	expression = input(" > ").replace("add","+").replace("substract","-").replace("multiple","*").replace("divide","/").replace("plus","+").replace("minus","-").replace("multiplied by","*").replace("divided by","/")
+	
 	# calculating normal expressions
 	if "+" in expression or "-" in expression or "*" in expression or "/" in expression :
 		try:
 			print(" result > ", eval(expression))
 		except Exception as e :
 			print(e)
+						
 			
 	# 2D figures
+
 
 	# square
 	elif "area" in expression and "of" in expression and "square" in expression :
@@ -23,7 +26,7 @@ while True :
 	elif "perimeter" in expression and "of" in expression and "square" in expression :
 		side = float(input("\n enter side = "))
 		print(" perimeter of square with side ",side,"=",4*side)
-
+		
 	# rectangle
 	elif "area" in expression and "of" in expression and "rectangle" in expression:
 		length = float(input("\n enter length = "))
@@ -33,7 +36,7 @@ while True :
 		length = float(input("\n enter length = "))
 		width =  float(input(" enter width = "))
 		print(" perimeter of rectangle with given dimensions  =  ", 2*(length+width))
-	
+		
 	# triangle
 	elif "area" in expression and "of" in expression and "triangle" in expression:
 		radius = float(input("\n radius = "))
@@ -47,13 +50,51 @@ while True :
 	# sector
 	elif "area" in expression and "of" in expression and "sector" in expression:
 		thetha = float(input("\n angle ⁰ = "))
-		radius = float(input(" radius of sector \ length of arm = "))
+		radius = float(input(" radius of sector or length of arm = "))
 		area = (thetha/360)*(3.14)*(radius*radius)
 		print(" area of given sector = " , area)
 	elif "perimeter" in expression and "of" in expression and "sector" in expression:
 		thetha = float(input("\n angle ⁰ = "))
-		radius = float(input(" radius of sector \ length of arm = "))
+		radius = float(input(" radius of sector or length of arm = "))
 		perimeter = (thetha/360)*(3.14)*(2*radius) + 2*radius
 		print(" area of given sector = " , area)
 	
+	
+	
 	# 3D figures 
+	
+	
+	# cube 
+	elif "volume" in expression and "of" in expression and "cube" in expression:
+		side = float(input("\n side of cube = "))
+		volume = pow(side,3)
+		print(" Volume of cube = " , volume)
+	elif "total" in expression and "surface" in expression and "area" in expression and "of" in expression and "cube" in expression:
+		side = float(input("\n side of cube = "))
+		tsa_cube = 6*(pow(side,2))
+		print(" Total Surface Area of Cube = ", tsa_cube)
+	elif "curved" in  expression and "surface" in expression and "area" in expression and "of" in expression and "cube" in expression:
+		side = float(input("\n side of cube = "))
+		csa_cube = 4*(pow(side,2))
+		print(" Curve surface area of cube = ")
+		
+	# cuboid
+	elif "volume" in expression and "of" in expression and "cuboid" in expression:
+		length = float(input("\n length of cuboid = "))
+		width = float(input(" width of cuboid = "))
+		height = float(input(" height of cuboid = "))
+		print(" Volume of cuboid = ", length*width*height)
+	elif "total" in expression and "surface" in expression and "area" in expression and "of" in expression and "cuboid" in expression:
+		length = float(input("\n length of cuboid = "))
+		width = float(input(" width of cuboid = "))
+		height = float(input(" height of cuboid = "))
+		tsa_cuboid = 2*(length*width + width*height + length*height)
+		print(" Total surface area of cuboid = ", tsa_cuboid)
+	elif "curved" in expression and "surface" in expression and "area" in expression and "of" in expression and "cuboid" in expression:
+		length = float(input("\n length of cuboid = "))
+		width = float(input(" width of cuboid = "))
+		height = float(input(" height of cuboid = "))
+		csa_cuboid = 2*(length*height + height*width)
+		print(" Curved surface area of cuboid = ",csa_cuboid)		
+	elif "quit" in expression or "exit" in expression:
+		break # exited on break
